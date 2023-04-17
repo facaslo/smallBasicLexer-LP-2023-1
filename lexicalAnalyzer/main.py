86da0ccd43a7c52124b3491523942f7471c4d39c
= <=>
2023-04-16 19:18:26 -0500

    syntax analyzer

---
+++ lexicalAnalyzer/main.py
@@ -0,0 +1,13 @@
+from lexer import Lexer
+
+if __name__ == "__main__":
+    l = Lexer()
+    while(True):      
+      token = l.readNextToken()
+      if token == None:
+          break        
+      print(token)
+
+    lexError = l.returnLexError()
+    if lexError != None:
+        print(lexError)
